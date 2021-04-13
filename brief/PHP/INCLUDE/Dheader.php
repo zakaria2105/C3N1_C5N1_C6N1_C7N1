8ac53acf25e6f8c6050d17615545ac66e3b9d1cf
zakaria2105 <76808317+zakaria2105@users.noreply.github.com>
2021-04-13 15:04:51 +0000

    Delete brief directory

--- brief/PHP/INCLUDE/Dheader.php
+++
@@ -1,9 +0,0 @@
-
-    <nav class="header">
-        <h2 class="h-h2">WeDev</h2>
-        <ul class="h-liste">
-            <li class="h-li"><a class="h-link" href="logout.php">LOGOUT</a></li>
-            <li class="h-li"><a class="h-link" href="formation.php">FORMATION</a></li>
-            <li class="h-li"><a class="h-link" href="Dashboard.php">HOME</a></li>
-        </ul>
-    </nav>
